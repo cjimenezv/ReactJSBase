849ef6ebf7d8d9033745b3ce4fb4e1010c813a85
cjimenezv <39573792+cjimenezv@users.noreply.github.com>
2018-05-27 20:50:01 -0500

    Cambio 2

--- redux-example/src/App.js
+++ redux-example/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
         <Navbar inverse staticTop>
           <Navbar.Header>
             <Navbar.Brand>
-              <a href="#">Ecommerce</a>
+              <a href="#">Ecommerce 24</a>
             </Navbar.Brand>
           </Navbar.Header>
         </Navbar>
